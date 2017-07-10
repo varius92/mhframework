1892ae7863f7cd74eb3eb243fb77d49a0cffcdca
Andreas Seifert <a.seifert@makrohaus.de>
2017-07-10 15:59:41 +0200

    aufgeräumt

--- test/templates/icon.php
+++ test/templates/icon.php
@@ -24,7 +24,7 @@
     <br>
     <br>
     <i class="Icon-fa Icon-fa-address-book"></i>
-    <br> <i class="Icon-fa Icon-fa-address-book"></i>
+    <br> <i class="fa fa-address-book"></i>
     <br> <i class="Icon-fa Icon-fa-address-book"></i>
     <br> <i class="Icon-fa Icon-fa-address-book"></i>
     <br> <i class="Icon-fa Icon-fa-address-book"></i>
