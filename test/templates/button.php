4407880f303535c212717d91b280f2cb627d28e8
Andreas Seifert <a.seifert@makrohaus.de>
2017-07-11 15:17:08 +0200

    Aufgeräumt#4

---
+++ test/templates/button.php
@@ -0,0 +1,8 @@
+<main class="grid grid--container">
+    <h1>Button</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium at
+        atque aut beatae culpa deleniti    <code>.button {}</code> doloribus explicabo fugiat, placeat
+        porro praesentium qui quo repellat sit sunt vel veniam vitae. Totam.</p>
+
+
+</main>
--- test/templates/icon.php
+++ test/templates/icon.php
@@ -1,25 +1,3 @@
-<!doctype html>
-<!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>
-<html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js" lang=""> <!--<![endif]-->
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="../../webroot/scss/main.css">
-    <link rel="stylesheet" href="../scss/demo.css">
-</head>
-<body>
-
 <main class="grid grid--container">
     <br>
     <br>
@@ -32,5 +10,3 @@
 
 
 </main>
-</body>
-</html>
