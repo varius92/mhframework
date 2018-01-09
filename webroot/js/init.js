20548d336bce8fcb92e0b9b1045eb21c3837c7a9
Andreas Seifert <a.seifert@makrohaus.de>
2018-01-09 11:31:30 +0100

    #

--- webroot/js/init.js
+++ webroot/js/init.js
@@ -1,35 +1,6 @@
 $(function () {
 
-    // SmartMenus init
-    $('#main-menu').smartmenus({
-        noMouseOver: true,
-        subMenusMaxWidth: 'auto',
-        subMenusMinWidth: 'auto',
-    });
 
-    // SmartMenus mobile menu toggle button
-    var $mainMenuState = $('#main-menu-state');
-    if ($mainMenuState.length) {
-        // animate mobile menu
-        $mainMenuState.change(function (e) {
-            var $menu = $('#main-menu');
-            if (this.checked) {
-                $menu.hide().slideDown(250, function () {
-                    $menu.css('display', '');
-                });
-            } else {
-                $menu.show().slideUp(250, function () {
-                    $menu.css('display', '');
-                });
-            }
-        });
-        // hide mobile menu beforeunload
-        $(window).on('beforeunload unload', function () {
-            if ($mainMenuState[0].checked) {
-                $mainMenuState[0].click();
-            }
-        });
-    }
 
 
 
